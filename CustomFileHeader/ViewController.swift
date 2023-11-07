1ffe0a7c75158629b8621b51d76916dcdf1b65b3
Nitin <nbnitin.bhatia@gmail.com>
2023-11-07 11:53:17 +0530

    first commit...

---
+++ CustomFileHeader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomFileHeader
+//
+//  Created by Nitin Bhatia on 07/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CustomFileHeader/loremIpsum.swift
@@ -0,0 +1,3 @@
+//This is my custom file header. loremIpsum.swift, created on 07/11/23.
+
+import Foundation
